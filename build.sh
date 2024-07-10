3e0024b953e0dd0ebc4a60d24b185ef3461296a0
eliasnijs <elias.nijs@gmail.com>
2024-07-10 21:42:58 +0200

    first commit :)

---
+++ build.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+#!/bin/sh
+
+rm -r build
+mkdir -p build
+
+SOURCE="src/main.cpp"
+NAME="a.out"
+VERSION="00"
+
+# includes and libs
+INCS="${INCS} -I libs/base -I libs/glad -I libs/imgui"
+LIBS="-lm -lGL -lpthread -lglfw -lSDL2"
+
+# flags
+CPPFLAGS="${CPPFLAGS} -D_DEFAULT_SOURCE -D_BSD_SOURCE -D_POSIX_C_SOURCE=2 -DVERSION=\"${VERSION}\""
+CFLAGS="${CFLAGS} -g -Wall -Wno-deprecated-declarations -Wno-write-strings -Wno-writable-strings -Wno-unused-function -O3 ${INCS} ${CPPFLAGS}"
+
+# compiler
+CC="g++"
+
+# building
+echo "[build.sh]# building $NAME"
+echo ${CC} $CFLAGS -o "build/$NAME" $SOURCE $LIBS
+${CC} $CFLAGS -o "build/$NAME" $SOURCE $LIBS
+echo "[build.sh]# finished building"
---
+++ src/main.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <cstdint>
+
+#include <glad.h>
+#include <glad.c>
+#define GLFW_INCLUDE_NONE
+#include <GLFW/glfw3.h>
+#include <imgui.h>
+#include <backends/imgui_impl_glfw.h>
+#include <backends/imgui_impl_opengl3.h>
+
+#define ENABLE_PRINT  1
+#define ENABLE_ASSERT 1
+#include "utils/utils.h"
+
+
+
+
+I32
+main()
+{
+
+
+	return 0;
+}
+
